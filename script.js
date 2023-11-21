3cb9881d7fc22ae4cf4a7bfcbaa942f49730ec6e
josef gisis <jsquared.gisis@gmail.com>
2023-11-21 16:58:22 -0500

    third draft

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 function toggleMenu() {
     let hamburgerNavigation = document.querySelector('.hamburger-navigation');
+    let hamburgerBars = document.querySelectorAll('.bar')
+
     if (hamburgerNavigation.style.display === 'none') {
         hamburgerNavigation.style.display = 'block';
+        hamburgerBars.forEach(bar => bar.style.margin = '9px 0');
+        hamburgerBars.forEach(bar => bar.style.height = '5px');
     } else {
         hamburgerNavigation.style.display = 'none';
+        hamburgerBars.forEach(bar => bar.style.margin = '4px 0');
+        hamburgerBars.forEach(bar => bar.style.height = '5px');
     }
 }
+
+
+function updateClassHeight(className) {
+    let classOccurrences = document.querySelectorAll(className);
+    if (classOccurrences.length > 0) {
+        classOccurrences.forEach(classOccurrence => {
+            let cardWidth = classOccurrence.offsetWidth;
+            let cardHeight = cardWidth * 1.5;
+            classOccurrence.style.height = cardHeight + 'px';
+        });
+    }
+}
+
+function updateAllElementHeights() {
+    updateClassHeight('.grid-card');
+    updateClassHeight('.grid-card-logo');
+    updateClassHeight('.grid-card-text');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateAllElementHeights(); // Initial update on page load
+});
+
+window.addEventListener('resize', function () {
+    updateAllElementHeights(); // Update on window resize
+});
